5816d2007278f5ee725f2ba391d48fad2c30f391
GwangGeun <jgg0328@gmail.com>
2020-04-02 22:15:58 +0900

    Revert "Added HelloDto & HelloDtoTest"
    This reverts commit ea6ca13e8103e3d256f307450a1449cbda57b499.

--- src/main/java/book/standup/com/springboot/web/HelloController.java
+++ src/main/java/book/standup/com/springboot/web/HelloController.java
@@ -1,8 +1,6 @@
 package book.standup.com.springboot.web;
 
-import book.standup.com.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +9,5 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
-    }
-
-    @GetMapping("/hello/dto")
-    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
-        return new HelloResponseDto(name,amount);
     }
 }
