4c4eeb7a3ec75d9085d9cdfae5cd1fbabc69d803
GwangGeun <jgg0328@gmail.com>
2020-04-05 00:29:16 +0900

    Added JPA

---
+++ src/main/java/book/standup/com/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package book.standup.com.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
