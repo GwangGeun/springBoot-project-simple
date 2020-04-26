1e74944bb86535b908153627288203032e84a98c
GwangGeun <jgg0328@gmail.com>
2020-04-26 23:49:13 +0900

    Added jpa basic + jpa auditing login

--- src/main/java/book/standup/com/springboot/service/posts/PostsService.java
+++ src/main/java/book/standup/com/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package book.standup.com.springboot.service.posts;
 
+import book.standup.com.springboot.domain.posts.Posts;
 import book.standup.com.springboot.domain.posts.PostsRepository;
+import book.standup.com.springboot.web.dto.PostsResponseDto;
 import book.standup.com.springboot.web.dto.PostsSaveRequestDto;
+import book.standup.com.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,17 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/book/standup/com/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/book/standup/com/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,6 @@
 package book.standup.com.springboot.domain.posts;
 
+import org.assertj.core.util.Lists;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,6 +38,26 @@ public class PostsRepositoryTest {
         Posts posts = postsLists.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void registBaseTimeEntity() {
+
+        // given
+        LocalDateTime now = LocalDateTime.of(2020, 4, 5, 0, 0, 0);
+        postsRepository.save(Posts.builder().title("title").content("content").author("author").build());
+
+        // when
+        List<Posts> postsLists = postsRepository.findAll();
+
+        // then
+        Posts posts = postsLists.get(0);
+
+        System.out.println(">>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
 
     }
 
